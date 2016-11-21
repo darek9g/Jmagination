3b63c62c68cbc6d4678466db3cf6e1c67dffb2cf
darek9g <gut@wsisiz.edu.pl>
2016-11-21 08:36:47 +0100

    gui gui gui

--- src/Workspace.java
+++ src/Workspace.java
@@ -29,19 +29,46 @@ public class Workspace{
 
         mainPanel.setBackground(ConstantsInitializers.GUI_DRAWING_BG_COLOR);
 
-        imagePanel = new JPanel(new BorderLayout());
-        imagePanel.setBackground(ConstantsInitializers.GUI_DRAWING_BG_COLOR);
+        this.srcImageServer = null;
+
+    }
+
+    public Workspace(ImageServer srcImageServer) {
+        this();
+        setImageserver(srcImageServer);
+    }
+
+    public void setImageserver(ImageServer srcImageServer) {
+        this.srcImageServer = srcImageServer;
+
+        /*mainPanel.remove(imagePanel);
+        mainPanel.remove(operationsPanel);
+        mainPanel.remove(histogramPanel);*/
+        mainPanel.removeAll();
+
+        BufferedImage img = Operations.duplicateImageFunction(srcImageServer.getImg());
+
+        imagePanel = new GUIStyler.ImagePanel2(img);
+        imagePanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+
+        histogramPanel = new GUIStyler.ImagePanel(srcImageServer.getHistogram().createImg2());
+        histogramPanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+
+        operationsPanel=new GUIStyler.PresenterTabOperations2(srcImageServer.operationManager);
+        operationsPanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+
+        fillMainPanel();
+
+        rebuildPanel();
+
+        System.out.println(mainPanel.getSize());
+
+    }
+
+    public void rebuildPanel() {
+
         mainPanel.add(imagePanel);
-
-
-
-        histogramPanel = new JPanel(new BorderLayout());
-        histogramPanel.setBackground(ConstantsInitializers.GUI_DRAWING_BG_COLOR);
-//        histogramPanel.setPreferredSize(new Dimension(300,400));
         mainPanel.add(histogramPanel);
-
-        operationsPanel = new JPanel(new GridBagLayout());
-        operationsPanel.setBackground(ConstantsInitializers.GUI_DRAWING_BG_COLOR);
         mainPanel.add(operationsPanel);
 
 
@@ -55,48 +82,17 @@ public class Workspace{
         layout.putConstraint(SpringLayout.WEST, imagePanel, ConstantsInitializers.GUI_WORKSPACEWINDOW_GAP_SIZE, SpringLayout.EAST, operationsPanel);
         layout.putConstraint(SpringLayout.WEST, imagePanel, ConstantsInitializers.GUI_WORKSPACEWINDOW_GAP_SIZE, SpringLayout.EAST, histogramPanel);
         layout.putConstraint(SpringLayout.EAST, imagePanel, ConstantsInitializers.GUI_WORKSPACEWINDOW_GAP_SIZE, SpringLayout.EAST, mainPanel);
-//        layout.putConstraint(SpringLayout.NORTH, imagePanel, ConstantsInitializers.GUI_WORKSPACEWINDOW_GAP_SIZE, SpringLayout.NORTH, mainPanel);
         layout.putConstraint(SpringLayout.VERTICAL_CENTER , imagePanel, ConstantsInitializers.GUI_WORKSPACEWINDOW_GAP_SIZE, SpringLayout.VERTICAL_CENTER, mainPanel);
 
-        window.setVisible(true);
-
-        this.srcImageServer = null;
-
-    }
-
-    public Workspace(ImageServer srcImageServer) {
-        this();
-        setImageserver(srcImageServer);
-    }
-
-    public void setImageserver(ImageServer srcImageServer) {
-        this.srcImageServer = srcImageServer;
-
-        BufferedImage img = Operations.duplicateImageFunction(srcImageServer.getImg());
-
-        imagePanel.removeAll();
-        imagePanel.add(new GUIStyler.ImagePanel2(img),BorderLayout.CENTER);
-
-        //imagePanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        imagePanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
-
-        histogramPanel.removeAll();
-        histogramPanel.add(new GUIStyler.ImagePanel(srcImageServer.getHistogram().createImg2()),BorderLayout.CENTER);
-        //histogramPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        histogramPanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
-
-        operationsPanel.removeAll();
-        operationsPanel.add(new GUIStyler.PresenterTabOperations2(srcImageServer.operationManager),new GUIStyler.ParamsGrid(1, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
-//        operationsPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        operationsPanel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
-
-        //fillMainPanel();
 
         window.pack();
+
+        window.setPreferredSize(layout.preferredLayoutSize(mainPanel));
+        window.setMinimumSize(layout.preferredLayoutSize(mainPanel));
+
         window.repaint();
 
-        System.out.println(mainPanel.getSize());
-
+        window.setVisible(true);
     }
 
 
@@ -115,8 +111,8 @@ public class Workspace{
         Dimension newMainPanelSize = new Dimension((int)leftPaneWidth, (int) leftPanelHeight);
 
 
-        operationsPanel.setSize(newOperationsPanelSize);
-        histogramPanel.setSize(newHistogramPanelSize);
+        operationsPanel.setMaximumSize(newOperationsPanelSize);
+        histogramPanel.setMaximumSize(newHistogramPanelSize);
 
         mainPanel.setSize(newMainPanelSize);
 
